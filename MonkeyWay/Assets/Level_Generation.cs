647179ff80144dd254013ee36620472d2cdc451a
Laktik228 <work.laktionov@gmail.com>
2022-01-29 01:13:49 +0200

    random platform generation amount added with different positions per batch

--- MonkeyWay/Assets/Level_Generation.cs
+++ MonkeyWay/Assets/Level_Generation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Level_Generation : MonoBehaviour
@@ -21,6 +22,24 @@ public class Level_Generation : MonoBehaviour
 
     void SpawnPlatform(){
         //TODO Make it adjustable to the ground
-        Instantiate(platform, new Vector3(rightEdgeX + Random.Range(5, 10), Random.Range(1, 7), 0.0f), Quaternion.identity);
+        int numberToSpawn = Random.Range(1, 5);
+        List<int> itemPositionX = new List<int>();
+        List<int> itemPositionY = new List<int>();
+        foreach(int i in Enumerable.Range(1, numberToSpawn)){
+            int posX = (int)rightEdgeX + Random.Range(5, 10);
+            int posY = Random.Range(-2, 3);
+            
+            while(itemPositionX.Contains(posX)){
+                posX = (int)rightEdgeX + Random.Range(5, 10);
+            }
+            itemPositionX.Add(posX);
+
+            while(itemPositionY.Contains(posY)){
+                posY = Random.Range(-2, 3);
+            }
+            itemPositionY.Add(posY);
+
+            Instantiate(platform, new Vector3(posX, posY, 0.0f), Quaternion.identity);
+        }   
     }
 }
