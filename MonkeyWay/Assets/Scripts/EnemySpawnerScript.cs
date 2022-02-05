6df1bf099e792eebb071e869081307e0c74752d7
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2022-02-05 18:00:58 +0000

    Working Enemy Spawner

--- MonkeyWay/Assets/Scripts/EnemySpawnerScript.cs
+++ MonkeyWay/Assets/Scripts/EnemySpawnerScript.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class EnemySpawnerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
     void Start()
     {
         
@@ -13,6 +18,14 @@ public class EnemySpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-2f, 2f);
+            whereToSpawn = new Vector2 (randX, transform.position.y);
+            Instantiate (enemy, whereToSpawn, Quaternion.identity);
+        }
+        
         
     }
 }
