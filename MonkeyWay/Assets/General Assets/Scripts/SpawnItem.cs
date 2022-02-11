7b01662ea3dff67f98cd259415a36ff8940b78fd
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2022-02-11 11:49:13 +0000

    Basic Spawn Script

---
+++ MonkeyWay/Assets/General Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public float spawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    void Start()
+    {
+    }
+
+ 
+}
