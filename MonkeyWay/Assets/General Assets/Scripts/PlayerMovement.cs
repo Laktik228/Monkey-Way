240ded045a47ee3e01b6a3e7566b48ce9ee40819
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2022-02-21 16:22:25 +0000

    Walking and Idle animations

--- MonkeyWay/Assets/General Assets/Scripts/PlayerMovement.cs
+++ MonkeyWay/Assets/General Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
     float horizontalMove = 0f;
@@ -20,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
 
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump")) {
             jump = true;
         }
