d5085435be7c38c23a9e6850e798c8a7b96e1799
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2022-03-09 22:38:25 +0000

    remove print statement

--- MonkeyWay/Assets/General Assets/Scripts/TreeFall.cs
+++ MonkeyWay/Assets/General Assets/Scripts/TreeFall.cs
@@ -6,7 +6,6 @@ public class TreeFall : MonoBehaviour
 {
     void Update()
     {   
-        Debug.Log(GetComponent<Transform>().rotation.z);
         if (GetComponent<Transform>().rotation.z < 0) {
             GetComponent<Rigidbody2D>().mass = 5;
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
